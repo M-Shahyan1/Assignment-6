dc77dd1cdce1d8dfaddc528a9a8a5cc661526c87
Muhammad Shahyan <m.shahyan.edu@gmail.com>
2025-07-27 01:17:38 -0400

    added and tested test for a negative digit input. there were no changes made to the code for the test to run successfully

--- test_Fibonacci.py
+++ test_Fibonacci.py
@@ -22,3 +22,6 @@ def test_Fibonacci_4():
 
 def test_Fibonacci_ten():
     assert list(Fibonacci(10)) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+def test_Fibonacci_negative():
+    assert list(Fibonacci(-20)) == []
