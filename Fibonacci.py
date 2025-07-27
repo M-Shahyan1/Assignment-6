2872b3b2cce983cb7dfbb21eddbd5fdacbff7d3f
Muhammad Shahyan <m.shahyan.edu@gmail.com>
2025-07-27 00:49:09 -0400

    added the block if a non-integer is used for the digit value within Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,8 @@
 """
 Displays Digits within Fibonacci's sequence as a list
 """
+
+class Fibonacci:
+    def __init__(self, digits):
+        if not isinstance(digits, int):
+            raise ValueError
--- test_Fibonacci.py
+++ test_Fibonacci.py
@@ -1,3 +1,10 @@
 """
 Tests the Fibonacci module
 """
+import pytest
+from Fibonacci import Fibonacci
+
+def test_Fibonacci_integer():
+    with pytest.raises(ValueError):
+        Fibonacci('Moon')
+
